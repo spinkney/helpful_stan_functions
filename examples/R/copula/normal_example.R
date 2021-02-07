4a9c2cd7bd062d5941147f635f507177b5e0629d
Sean Pinkney <sean.pinkney@gmail.com>
2021-02-07 10:09:55 -0500

    better normal copula example

--- examples/R/copula/normal_example.R
+++ examples/R/copula/normal_example.R
@@ -1,14 +1,16 @@
 library(cmdstanr)
 library(copula)
+library(ExtDist)
+library(VGAM)
 
-fp <- file.path("./examples/stan/copula/normal_copula_example.stan")
+fp <- file.path("./examples/stan/copula/normal_copula_example_test.stan")
 mod <- cmdstan_model(fp, include_paths = "./functions/copula", 
                      force_recompile = T)
 
-G3 <- normalCopula(.37, dim=2)
-gMvd2 <- mvdc(G3, c("norm","norm"), param = 
+G3 <- normalCopula(-.3, dim=2)
+gMvd2 <- mvdc(G3, c("norm","Gumbel"), param = 
                 list(list(mean = 3.5, sd = 0.5), 
-                     list(mean = 2.75, sd = 0.75)))
+                     list(location = 0, scale = 1)))
 set.seed(11)
 
 N <- 500
@@ -18,7 +20,7 @@ mod_out <- mod$sample(
   data = list(N = N,
               x = x[, 1],
               y = x[, 2],
-              is_vector = 1),
+              is_vector = 0),
   chains = 2,
   adapt_delta = 0.8,
   parallel_chains = 2,
