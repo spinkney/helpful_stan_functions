e5080aa05a3aa68b3ef6f861f771d3770f41c370
Sean Pinkney <sean.pinkney@gmail.com>
2021-02-19 11:41:45 -0500

    Delete test directory

--- test/copula/R/bivariate_normal_copula_cdf_test.R
+++
@@ -1,8 +0,0 @@
-library(cmdstanr)
-fp <- file.path("./test/copula/bivariate_normal_copula_cdf.stan")
-mod <- cmdstan_model(fp, include_paths = c("./functions/void",
-                                           "./functions/copula"))
-
-my_samples <- mod$sample(chains = 1,
-                         iter_sampling = 1,
-                         fixed_param = T)
--- test/special/R/inv_erf_test.R
+++
@@ -1,9 +0,0 @@
-library(cmdstanr)
-fp <- file.path("test/special/inv_erf_test.stan")
-mod <- cmdstan_model(fp,  
-                     include_paths = c("./functions/special",
-                                       "./functions/void"))
-
-mod_out <- mod$sample(
-  fixed_param = TRUE
-)
