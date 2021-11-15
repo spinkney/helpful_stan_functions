684f60a59c76c4e0e6b4eb11cc683045c091a7a1
spinkney <spinkney@users.noreply.github.com>
2021-11-15 16:34:51 +0000

    deploy: fff1867aa33bb9d11855f6d8c6267b11c0fea528

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['skew_5fgeneralized_5ft_5flpdf_0',['skew_generalized_t_lpdf',['../group__skew__generalized__t.html#gaf9794f9e779f2354aa8390d5fec3ce86',1,'skew_generalized_t.stan']]],
+  ['skew_5fgeneralized_5ft_5flpdf_0',['skew_generalized_t_lpdf',['../group__skew__generalized__t.html#ga7a1f5a71101932c511f15e1379df4562',1,'skew_generalized_t.stan']]],
   ['skewed_5fgeneralized_5ft_5flcdf_1',['skewed_generalized_t_lcdf',['../group__skew__generalized__t.html#gad2bea362601fb9aef75f73199a3b73df',1,'skew_generalized_t.stan']]],
   ['skewed_5fgeneralized_5ft_5fqf_2',['skewed_generalized_t_qf',['../group__qf.html#gabba9d4d3227b92d49fae91c3ca6c6daf',1,'skewed_generalized_t_qf.stan']]]
 ];
