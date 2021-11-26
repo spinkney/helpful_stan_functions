52c38a391beb55075ea67ce089c85fcb4998f70e
spinkney <spinkney@users.noreply.github.com>
2021-11-26 11:51:41 +0000

    deploy: 82ef0075c722c57132c6ae16ed0a905c5247b7e6

--- search/all_8.js
+++ search/all_8.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['inc_5fbeta_5finverse_0',['inc_beta_inverse',['../inc__beta__inverse_8stanfunctions.html#a060fcd6515b67c752a9da49d0cc7f9e6',1,'inc_beta_inverse.stanfunctions']]],
-  ['inc_5fbeta_5finverse_2estanfunctions_1',['inc_beta_inverse.stanfunctions',['../inc__beta__inverse_8stanfunctions.html',1,'']]],
-  ['interp_5f1d_5fcubic_2',['interp_1d_cubic',['../group__d__interpolation.html#ga04f55d73f3519184cb0756246c4a869e',1,'interp_1d_cubic.stanfunctions']]],
-  ['interp_5f1d_5fcubic_2estanfunctions_3',['interp_1d_cubic.stanfunctions',['../interp__1d__cubic_8stanfunctions.html',1,'']]],
-  ['interp_5f1d_5flinear_4',['interp_1d_linear',['../group__d__interpolation.html#gaf1b52e91870caffdf1766eca0c75421f',1,'interp_1d_linear.stanfunctions']]],
-  ['interp_5f1d_5flinear_2estanfunctions_5',['interp_1d_linear.stanfunctions',['../interp__1d__linear_8stanfunctions.html',1,'']]],
-  ['interpolation_20functions_6',['Interpolation Functions',['../group__interpolation.html',1,'']]],
-  ['interpolation_5fdefinition_2estan_7',['interpolation_definition.stan',['../interpolation__definition_8stan.html',1,'']]],
-  ['inv_5ferf_8',['inv_erf',['../inv__erf_8stanfunctions.html#a3d254497bd04ee837661a923dff361ba',1,'inv_erf.stanfunctions']]],
-  ['inv_5ferf_2estanfunctions_9',['inv_erf.stanfunctions',['../inv__erf_8stanfunctions.html',1,'']]],
-  ['inv_5ferf_5fnsqrt_10',['inv_erf_nsqrt',['../inv__erf_8stanfunctions.html#a33132970dd1c67785771c3af2dcad1ee',1,'inv_erf.stanfunctions']]]
+  ['in_5forder_0',['in_order',['../group__logical__array.html#ga2becc165c804319fd033bc82a5c13ff6',1,'logical_array.stanfunctions']]],
+  ['inc_5fbeta_5finverse_1',['inc_beta_inverse',['../inc__beta__inverse_8stanfunctions.html#a060fcd6515b67c752a9da49d0cc7f9e6',1,'inc_beta_inverse.stanfunctions']]],
+  ['inc_5fbeta_5finverse_2estanfunctions_2',['inc_beta_inverse.stanfunctions',['../inc__beta__inverse_8stanfunctions.html',1,'']]],
+  ['interp_5f1d_5fcubic_3',['interp_1d_cubic',['../group__d__interpolation.html#ga04f55d73f3519184cb0756246c4a869e',1,'interp_1d_cubic.stanfunctions']]],
+  ['interp_5f1d_5fcubic_2estanfunctions_4',['interp_1d_cubic.stanfunctions',['../interp__1d__cubic_8stanfunctions.html',1,'']]],
+  ['interp_5f1d_5flinear_5',['interp_1d_linear',['../group__d__interpolation.html#gaf1b52e91870caffdf1766eca0c75421f',1,'interp_1d_linear.stanfunctions']]],
+  ['interp_5f1d_5flinear_2estanfunctions_6',['interp_1d_linear.stanfunctions',['../interp__1d__linear_8stanfunctions.html',1,'']]],
+  ['interpolation_20functions_7',['Interpolation Functions',['../group__interpolation.html',1,'']]],
+  ['interpolation_5fdefinition_2estan_8',['interpolation_definition.stan',['../interpolation__definition_8stan.html',1,'']]],
+  ['inv_5ferf_9',['inv_erf',['../inv__erf_8stanfunctions.html#a3d254497bd04ee837661a923dff361ba',1,'inv_erf.stanfunctions']]],
+  ['inv_5ferf_2estanfunctions_10',['inv_erf.stanfunctions',['../inv__erf_8stanfunctions.html',1,'']]],
+  ['inv_5ferf_5fnsqrt_11',['inv_erf_nsqrt',['../inv__erf_8stanfunctions.html#a33132970dd1c67785771c3af2dcad1ee',1,'inv_erf.stanfunctions']]]
 ];
