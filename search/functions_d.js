a05929e90f91f34a516b797d85030a1595ddda8e
spinkney <spinkney@users.noreply.github.com>
2021-12-10 19:17:05 +0000

    deploy: bd3a592b79f589ad4ddef40422158e6906118b95

--- search/functions_d.js
+++ search/functions_d.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['skew_5fgeneralized_5ft_5flpdf_0',['skew_generalized_t_lpdf',['../group__skew__generalized__t.html#ga7a1f5a71101932c511f15e1379df4562',1,'skew_generalized_t.stanfunctions']]],
   ['skewed_5fgeneralized_5ft_5flcdf_1',['skewed_generalized_t_lcdf',['../group__skew__generalized__t.html#gad2bea362601fb9aef75f73199a3b73df',1,'skew_generalized_t.stanfunctions']]],
-  ['skewed_5fgeneralized_5ft_5fqf_2',['skewed_generalized_t_qf',['../group__qf.html#gace14f485984c8e7c50b0d11a6c461c94',1,'skewed_generalized_t_qf.stanfunctions']]]
+  ['skewed_5fgeneralized_5ft_5fqf_2',['skewed_generalized_t_qf',['../group__qf.html#ga82a3bccf3f464eb76782e6b27f24dbe3',1,'skewed_generalized_t_qf.stanfunctions']]]
 ];
--- skewed__generalized__t__qf_8stanfunctions.js
+++ skewed__generalized__t__qf_8stanfunctions.js
@@ -1,4 +1,4 @@
 var skewed__generalized__t__qf_8stanfunctions =
 [
-    [ "skewed_generalized_t_qf", "group__qf.html#gace14f485984c8e7c50b0d11a6c461c94", null ]
+    [ "skewed_generalized_t_qf", "group__qf.html#ga82a3bccf3f464eb76782e6b27f24dbe3", null ]
 ];
