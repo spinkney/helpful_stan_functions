a3612e467d07b9d84537bd789bd90460bcbd606e
spinkney <spinkney@users.noreply.github.com>
2022-03-10 12:00:00 +0000

    deploy: 71adb08c39463fb0b12755607e939124ea0e7388

--- search/files_6.js
+++ search/files_6.js
@@ -4,6 +4,5 @@ var searchData=
   ['interp_5f1d_5fcubic_2estanfunctions_1',['interp_1d_cubic.stanfunctions',['../interp__1d__cubic_8stanfunctions.html',1,'']]],
   ['interp_5f1d_5flinear_2estanfunctions_2',['interp_1d_linear.stanfunctions',['../interp__1d__linear_8stanfunctions.html',1,'']]],
   ['interpolation_5fdefinition_2estan_3',['interpolation_definition.stan',['../interpolation__definition_8stan.html',1,'']]],
-  ['inv_5fbeta_5fori_2estanfunctions_4',['inv_beta_ori.stanfunctions',['../inv__beta__ori_8stanfunctions.html',1,'']]],
-  ['inv_5ferf_2estanfunctions_5',['inv_erf.stanfunctions',['../inv__erf_8stanfunctions.html',1,'']]]
+  ['inv_5ferf_2estanfunctions_4',['inv_erf.stanfunctions',['../inv__erf_8stanfunctions.html',1,'']]]
 ];
