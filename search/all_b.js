151a50f138e7824f8514dc9a5d321e60d88f23ce
spinkney <spinkney@users.noreply.github.com>
2022-03-12 13:16:08 +0000

    deploy: e666963c15f21b6f180462cf011694aa482ab81c

--- search/all_b.js
+++ search/all_b.js
@@ -9,11 +9,10 @@ var searchData=
   ['multi_5fnormal_5fcholesky_5ftruncated_5flp_6',['multi_normal_cholesky_truncated_lp',['../group__tmvn.html#ga7c9f3672e2ad840066dae356576cb137',1,'multi_normal_cholesky_truncated.stanfunctions']]],
   ['multi_5fnormal_5fcholesky_5ftruncated_5flpdf_7',['multi_normal_cholesky_truncated_lpdf',['../group__tmvn.html#ga410a62b317befea7a30c079292d142f0',1,'multi_normal_cholesky_truncated.stanfunctions']]],
   ['multi_5fnormal_5fcholesky_5ftruncated_5frng_8',['multi_normal_cholesky_truncated_rng',['../group__tmvn.html#gaa5f97f79d524abf4ecf3e532a3dfb812',1,'multi_normal_cholesky_truncated.stanfunctions']]],
-  ['multi_5fnormal_5fcopula_5flpdf_9',['multi_normal_copula_lpdf',['../group__normal.html#ga2aac932cd087e9111ca72c895ee6fbe0',1,'normal_copula.stanfunctions']]],
-  ['multi_5fwallenius_5fhypergeometric_2estanfunctions_10',['multi_wallenius_hypergeometric.stanfunctions',['../multi__wallenius__hypergeometric_8stanfunctions.html',1,'']]],
-  ['multi_5fwallenius_5fintegral_11',['multi_wallenius_integral',['../group__multi__wallenius.html#gaad1c6d45dab33e14585efdd01a763f85',1,'multi_wallenius_hypergeometric.stanfunctions']]],
-  ['multi_5fwallenius_5flpmf_12',['multi_wallenius_lpmf',['../group__multi__wallenius.html#gac1b1c12f44019e532e76219190c0d96c',1,'multi_wallenius_hypergeometric.stanfunctions']]],
-  ['multivariate_20distributions_13',['Multivariate Distributions',['../group__multivariate.html',1,'']]],
-  ['multivariate_20wallenius_27_20noncentral_20hypergeometric_20distribution_20functions_14',['Multivariate Wallenius&apos; Noncentral Hypergeometric Distribution Functions',['../group__multi__wallenius.html',1,'']]],
-  ['multivariate_5fdefinition_2estan_15',['multivariate_definition.stan',['../multivariate__definition_8stan.html',1,'']]]
+  ['multi_5fwallenius_5fhypergeometric_2estanfunctions_9',['multi_wallenius_hypergeometric.stanfunctions',['../multi__wallenius__hypergeometric_8stanfunctions.html',1,'']]],
+  ['multi_5fwallenius_5fintegral_10',['multi_wallenius_integral',['../group__multi__wallenius.html#gaad1c6d45dab33e14585efdd01a763f85',1,'multi_wallenius_hypergeometric.stanfunctions']]],
+  ['multi_5fwallenius_5flpmf_11',['multi_wallenius_lpmf',['../group__multi__wallenius.html#gac1b1c12f44019e532e76219190c0d96c',1,'multi_wallenius_hypergeometric.stanfunctions']]],
+  ['multivariate_20distributions_12',['Multivariate Distributions',['../group__multivariate.html',1,'']]],
+  ['multivariate_20wallenius_27_20noncentral_20hypergeometric_20distribution_20functions_13',['Multivariate Wallenius&apos; Noncentral Hypergeometric Distribution Functions',['../group__multi__wallenius.html',1,'']]],
+  ['multivariate_5fdefinition_2estan_14',['multivariate_definition.stan',['../multivariate__definition_8stan.html',1,'']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -5,7 +5,6 @@ var searchData=
   ['multi_5fnormal_5fcholesky_5ftruncated_5flp_2',['multi_normal_cholesky_truncated_lp',['../group__tmvn.html#ga7c9f3672e2ad840066dae356576cb137',1,'multi_normal_cholesky_truncated.stanfunctions']]],
   ['multi_5fnormal_5fcholesky_5ftruncated_5flpdf_3',['multi_normal_cholesky_truncated_lpdf',['../group__tmvn.html#ga410a62b317befea7a30c079292d142f0',1,'multi_normal_cholesky_truncated.stanfunctions']]],
   ['multi_5fnormal_5fcholesky_5ftruncated_5frng_4',['multi_normal_cholesky_truncated_rng',['../group__tmvn.html#gaa5f97f79d524abf4ecf3e532a3dfb812',1,'multi_normal_cholesky_truncated.stanfunctions']]],
-  ['multi_5fnormal_5fcopula_5flpdf_5',['multi_normal_copula_lpdf',['../group__normal.html#ga2aac932cd087e9111ca72c895ee6fbe0',1,'normal_copula.stanfunctions']]],
-  ['multi_5fwallenius_5fintegral_6',['multi_wallenius_integral',['../group__multi__wallenius.html#gaad1c6d45dab33e14585efdd01a763f85',1,'multi_wallenius_hypergeometric.stanfunctions']]],
-  ['multi_5fwallenius_5flpmf_7',['multi_wallenius_lpmf',['../group__multi__wallenius.html#gac1b1c12f44019e532e76219190c0d96c',1,'multi_wallenius_hypergeometric.stanfunctions']]]
+  ['multi_5fwallenius_5fintegral_5',['multi_wallenius_integral',['../group__multi__wallenius.html#gaad1c6d45dab33e14585efdd01a763f85',1,'multi_wallenius_hypergeometric.stanfunctions']]],
+  ['multi_5fwallenius_5flpmf_6',['multi_wallenius_lpmf',['../group__multi__wallenius.html#gac1b1c12f44019e532e76219190c0d96c',1,'multi_wallenius_hypergeometric.stanfunctions']]]
 ];
