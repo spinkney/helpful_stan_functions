3959306ea6f03e1f63bd7129e2aa4bb8ccaa160e
spinkney <spinkney@users.noreply.github.com>
2022-03-24 17:54:27 +0000

    deploy: fc7c59930e0c170a1e5ca8b4dff203e60875a5f4

--- group__normal.js
+++ group__normal.js
@@ -1,6 +1,6 @@
 var group__normal =
 [
-    [ "bivariate_normal_copula_cdf", "group__normal.html#ga0cfca0aba6b1049472eac7cafe169143", null ],
+    [ "bivariate_normal_copula_cdf", "group__normal.html#gaf774c87e4771d60525a50a5d94391516", null ],
     [ "multi_normal_cholesky_copula_lpdf", "group__normal.html#ga53b0ac15ddf226fa86c96800014d89fc", null ],
     [ "normal_copula_lpdf", "group__normal.html#gaeceb683df43c85469ececa537d2e2266", null ],
     [ "normal_copula_vector_lpdf", "group__normal.html#ga09447a56c60491b2e53ed77922e68ece", null ]
