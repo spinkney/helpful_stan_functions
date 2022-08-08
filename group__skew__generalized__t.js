34d11c295f44d3cd8d9ffd2b1a39c7b8c0ef1669
spinkney <spinkney@users.noreply.github.com>
2022-08-08 14:19:23 +0000

    deploy: 395846aa59089ff4b91c2f53ad2ec7e4064ff4d8

--- group__skew__generalized__t.js
+++ group__skew__generalized__t.js
@@ -1,5 +1,6 @@
 var group__skew__generalized__t =
 [
+    [ "mean_centered_sgt", "group__skew__generalized__t.html#ga4492987a953344d6399268686734a9eb", null ],
     [ "mean_centered_sgt", "group__skew__generalized__t.html#ga8f03167e434b644f63f456087af5f150", null ],
     [ "skew_generalized_t_lcdf", "group__skew__generalized__t.html#ga6ea5a0033c7d222a2b010214e5708a8a", null ],
     [ "skew_generalized_t_lpdf", "group__skew__generalized__t.html#ga7a1f5a71101932c511f15e1379df4562", null ],
